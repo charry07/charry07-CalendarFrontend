f0d37c63f2a93bbcb1b02f7c5beb3f506a7dc390
charry07 <charry072013@gmail.com>
2023-07-10 10:20:11 -0500

    Calendar Api produccion URL

--- src/api/CalendarApi.ts
+++ src/api/CalendarApi.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-
 export const CalendarApi = axios.create({
   baseURL: `http://localhost:3001/api`,
+  // baseURL: `https://calendarbackend-wjeq.onrender.com/api`,
   headers: {
     'x-token': localStorage.getItem('token') || '',
     'Content-Type': 'application/json',
